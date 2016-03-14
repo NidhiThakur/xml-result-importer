6dc6897a7ecdbeb8acc78f7b35c9971fc56367c8
NidhiThakur <nidhia@gmail.com>
2016-03-14 16:57:52 +0000

    updated the batch file
    updated the batch file

--- Run.bat
+++ Run.bat
@@ -1,4 +1,4 @@
-<Project name> <test plan Number> <path to input file> <Name for test run>
+<TFS Server> <Project name> <test plan Number> <path to input file> <Name for test run>
 
 REM XMLResultImporter.exe "https://xxx.visualstudio.com/DefaultCollection" XXX XXXXX "C\XMLResultImporter\Input.xml" SpecflowTestRun
 
